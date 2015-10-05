2d3b3072a09f800253632fd08373b232905419a8
pauEscarcia <paulina_151090@hotmail.com>
2015-10-05 01:04:49 -0500

    DataFormat

--- section2/2.1 Bar Charts/Data Format.R
+++ section2/2.1 Bar Charts/Data Format.R
@@ -2,3 +2,10 @@ mm.counts <- c(12,6,8,10,6,7)
 names(mm.counts) <- c("blue","brown","green","orange",
                       "red","yellow")
 mm.counts
+mm.colors <- c("blue","brown","green","orange","red","yellow")
+getwd()
+setwd("/home/pauescarcia/Documentos/BigDataZacatecas/BigData-Zacatecas/section2/2.1 Bar Charts")
+mm.df <- read.csv("mnm-clean.csv",header=T)
+head(mm.df)
+par(mfrow=c(1,1))
+barplot(mm.counts,main="My M&M Color Distribution",xlab="M&MColors",ylab="Number of M&Ms in Bag",col=mm.colors)
