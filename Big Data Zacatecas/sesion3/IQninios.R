0466a557b5c22ba40d6e6e45a5f4a28bf62b99cc
pauEscarcia <paulina_151090@hotmail.com>
2015-10-06 23:11:35 -0500

    IQ de ninios

---
+++ Big Data Zacatecas/sesion3/IQninios.R
@@ -0,0 +1,24 @@
+# Children's IQ scores are normally distributed with a
+# mean of 100 and a standard deviation of 15. What
+# proportion of children are expected to have an IQ between
+# 80 and 120?
+
+mean=100; sd=15
+lb=80; ub=120
+
+x <- seq(-4,4,length=100)*sd + mean
+hx <- dnorm(x,mean,sd)
+hx
+#Ponemos las labels
+plot(x, hx, type="n", xlab="Valores IQ ", ylab="",
+     main="Distribucion Normal", axes=FALSE)
+
+i <- x >= lb & x <= ub
+lines(x, hx)
+polygon(c(lb,x[i],ub), c(0,hx[i],0), col="red")
+
+area <- pnorm(ub, mean, sd) - pnorm(lb, mean, sd)
+result <- paste("P(",lb,"< IQ <",ub,") =",
+                signif(area, digits=3))
+mtext(result,3)
+axis(1, at=seq(40, 160, 20), pos=0) 
