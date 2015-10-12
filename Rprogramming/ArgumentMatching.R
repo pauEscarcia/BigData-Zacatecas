c2d9ab0698750c22798d40f802f33f46bd115e51
pauEscarcia <paulina_151090@hotmail.com>
2015-10-12 12:53:05 -0500

    Argument Matching

--- Rprogramming/ArgumentMatching.R
+++ Rprogramming/ArgumentMatching.R
@@ -10,5 +10,4 @@ sd(x=mydata,na.rm = FALSE)
 sd(na.rm = FALSE, x = mydata)
 sd(na.rm = FALSE, mydata)
 args(lm)
-lm(data = mydata, y ~ x, model = FALSE, 1:100)
-lm(y ~ x, mydata, 1:100, model = FALSE)
+git 
