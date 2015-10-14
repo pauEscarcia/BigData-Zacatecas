7ebe5b5d2f47515c68b763aae68780717b466b4c
pauEscarcia <paulina_151090@hotmail.com>
2015-10-14 10:36:52 -0500

    Pie Chart

---
+++ Big Data Zacatecas/sesion6/tarea5PieChart.R
@@ -0,0 +1,12 @@
+#Comparative Box Plots
+all.scores <- read.csv("bafijaporcada100habitantes.csv")
+all.scores
+suscripciones <- all.scores[,"Suscripciones.100.h"] #trae columnas 
+sus <- suscripciones[10:15]
+sus
+lbls <- c("2009","2010","2011","2012","2013","2014")
+pct <- round(sus/sum(suscripciones)*100)
+lbls <- paste(lbls, pct) # add percents to labels
+lbls <- paste(lbls,"%",sep="") # ad % to labels 
+  
+pie(sus,labels=lbls,main="Banda Ancha Fija por cada 100 habitantes",col=rainbow(length(lbls)))
