27fe708dcb97ddcab6c8b52b77227aae1c0b1c31
pauEscarcia <paulina_151090@hotmail.com>
2015-10-14 08:15:04 -0500

    Histogram

---
+++ Big Data Zacatecas/sesion6/tarea2Histogram.R
@@ -0,0 +1,7 @@
+getwd()
+setwd("sesion6")
+all.scores <- read.csv("bafijaporcada100habitantes.csv")
+all.scores
+
+#Realizar un histograma con todos los datos de los suscriptores
+hist(suscripciones)
