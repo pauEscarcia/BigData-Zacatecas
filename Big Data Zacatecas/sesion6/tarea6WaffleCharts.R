5dbd38ad17c0514a01354332a4d3dbdabf21eeb7
pauEscarcia <paulina_151090@hotmail.com>
2015-10-14 11:46:43 -0500

    WaffleCharts

--- Big Data Zacatecas/sesion6/tarea6WaffleCharts.R
+++ Big Data Zacatecas/sesion6/tarea6WaffleCharts.R
@@ -6,6 +6,6 @@ sus <- suscripciones[5:10]
 sus
 #anio <- c("2004","2005","2006","2007","2008","2009")
 #total <- cbind(sus[i],anio[i])
-#total
+#total  
 waffle::waffle(total, rows=5, colors=rainbow(length(sus)), title="Banda Ancha Fija por cada 100 habitantes",
                xlab="Suscriptores")
