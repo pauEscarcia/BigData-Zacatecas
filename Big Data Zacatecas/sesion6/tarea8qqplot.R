36ff3503067acf1c91bb5412c1b649021821a6e0
pauEscarcia <paulina_151090@hotmail.com>
2015-10-14 12:26:34 -0500

    QQ Plots

---
+++ Big Data Zacatecas/sesion6/tarea8qqplot.R
@@ -0,0 +1,9 @@
+par(mfrow=c(1,2))
+all.scores <- read.csv("bafijaporcada100habitantes.csv")
+all.scores
+suscripciones <- all.scores[,"Suscripciones"] #trae columna
+suscripciones
+qqnorm(suscripciones); qqline(suscripciones)
+qqplot(x, x, main="t(3) Q-Q Plot",
+       ylab="Banda Ancha Fija")
+abline(0,1) 
