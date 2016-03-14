6ed59608d4583f8d0bdb5caa55c80f41a1c3844a
PauEscarcia <paulina_151090@hotmail.com>
2016-03-13 21:18:03 -0600

    Change of Type

--- MachineLearningStandford/chemical.R
+++ MachineLearningStandford/chemical.R
@@ -5,6 +5,6 @@ x <- all.scores[,"x"]
 y <- all.scores[,"y"]
 y
 par(mfrow=c(1,1))
-plot(x, y, xlab="eje de abscisas", ylab="eje de ordenadas", main="Chemical", sub="ejemplo")
+plot(x, y, xlab="eje de abscisas", ylab="eje de ordenadas", main="Chemical", sub="ejemplo",type="overplotted",col="pink")
 
 
